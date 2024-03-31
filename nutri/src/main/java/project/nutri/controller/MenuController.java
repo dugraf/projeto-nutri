9cc3e4adb3c93e772c7fdf5c7f480ca9cc4eab1d
Eduardo Rodrigues Graf <eduardor.graf@hotmail.com>
2024-03-30 23:47:35 -0300

    Ajuste MenuController -> Usuários
    •	Ajustado MenuController.java e MenuView.fxml;
    •	Criação do UserFormController.java, UserForm.fxml e UserSearch.fxml;
    •	Criação da classe project.nutri.controller.util.CallWindow, para invocar uma nova tela.

--- nutri/src/main/java/project/nutri/controller/MenuController.java
+++ nutri/src/main/java/project/nutri/controller/MenuController.java
@@ -1,6 +1,31 @@
 package project.nutri.controller;
 
+import org.springframework.stereotype.Component;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.MenuItem;
+import project.nutri.controller.util.CallWindow;
+
+@Component
 public class MenuController
 {
-    
+    @FXML
+    private MenuItem menuItemCreate;
+
+    @FXML
+    private MenuItem menuItemSearch;
+
+    private CallWindow callWindow = new CallWindow();
+
+    @FXML
+    public void onMenuItemCreateAction()
+    {
+        callWindow.openWindow("/templates/UserForm.fxml", "Cadastro de Usuário");
+    }
+
+    @FXML
+    public void onMenuItemSearchAction()
+    {
+        callWindow.openWindow("/templates/UserSearch.fxml", "Usuários de sistema");
+    }
 }
---
+++ nutri/src/main/java/project/nutri/controller/util/CallWindow.java
@@ -0,0 +1,28 @@
+package project.nutri.controller.util;
+
+import java.io.IOException;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class CallWindow
+{
+    public void openWindow(String fxmlPath, String title)
+    {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(fxmlPath));
+            Parent root = fxmlLoader.load();
+            Image icon = new Image(getClass().getResourceAsStream("/templates/imgs/nutrition.png"));
+            Stage stage = new Stage();
+            stage.setTitle(title);
+            stage.getIcons().add(icon);
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
